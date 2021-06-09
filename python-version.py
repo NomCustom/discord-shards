c33f6b151c2ba58af7abdc2b3f6610d34a138e95
NomCustom <72727437+NomCustom@users.noreply.github.com>
2021-06-09 13:40:27 +0300

    Update python-version.py

--- python-version.py
+++ python-version.py
@@ -12,3 +12,15 @@ async def on_ready():
   print(f"Shards activated! Number of shards: {bot.shard_count - 1}.")
 
 bot.run(your_bot_token)
+
+# custom client version
+
+class YourCustomClientName(commands.AutoShardedBot):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+  async def on_ready(self):
+    print("Custom bot client is ready")
+    
+bot = YourCustomClient(command_prefix=prefix)
+
+bot.run("your_token_here")
